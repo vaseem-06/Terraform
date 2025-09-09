aa4f8044c9e3850d302fe2f2fa25416c2b19c42e
vaseem-06 <vaseem73300@gmail.com>
2025-09-09 17:42:14 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,8 @@
+variable "filename" {
+  description = "The name of the file to create"
+  type        = string
+}
+variable "content" {
+  description = "The content to put inside the file"
+  type        = string
+}
